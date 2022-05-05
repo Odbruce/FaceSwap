dec4f4b1156969cc46f5bdebc913f278aadffe98
Odbruce <odera996@gmail.com>
2022-05-05 11:48:39 +0100

    app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import Navigation from "./Components/Navigation";
 import { Home,Memes,Register,SignIn,Instructions } from "./pages";
 import PrivateRoute from "./Components/PrivateRoute";
-import { Routes, Route, Navigate, useMatch } from "react-router-dom";
-import Particles from "react-tsparticles";
+import { Routes, Route, Navigate} from "react-router-dom";
 import { useFireContext } from "./Components/FirebaseContext";
 import { ContextProvider } from "./Components/ContextProvider/ContextProvider";
 
@@ -13,77 +12,7 @@ const App = ()=>{
   return (
     <>
       <div className="app">
-        {/* <Particles
-      id="tsparticles"    
-      options={{
-        fpsLimit: 120,
-        interactivity: {
-          events: {
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 1,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 100,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color:"#332333",
-            distance: 150,
-            enable: true,
-            opacity: 0.3,
-            width: 1,
-          },
-          collisions: {
-            enable: false,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 4,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 2,
-          },
-        },
-        detectRetina: true,
-      }}
-    />  */}
+       
     <ContextProvider addMeme={addMeme} user={user} >
         <Navigation />
         <Routes>
